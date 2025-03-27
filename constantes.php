a589ef0a602bd10311cb78f0e8fe173b5f5f1af8
franncolom <franciscocolom000@gmail.com>
2025-03-27 15:28:45 -0300

    Agregando constantes.php

---
+++ constantes.php
@@ -0,0 +1,15 @@
+<?php
+
+const PI = 3.1416;
+//const PI = 3.14;
+
+/* 
+  1. ¿Qué error arrojó PHP al intentar cambiar el valor de la constante PI?
+  2. ¿Qué nos indica este error sobre el comportamiento de las constantes en PHP?
+  3. ¿En qué situaciones seria util utilizar constantes en lugar de variables?
+
+  1. PHP nos da error de declaracion de constante duplicada, ya que no puede haber dos constantes con el mismo nombre.
+  2. Este error nos indica que PHP no permite cambiar el valor de una constante, por lo que no se puede cambiar el valor de la constante PI.
+  3. Las constantes son variables, que no se pueden modificar, por lo que en este caso lo recomendable es usar variable para poder modificar el valor de la constante.
+*/
+?>
