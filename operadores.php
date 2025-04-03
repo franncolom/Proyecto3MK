f6e0ed6844af942777d08bc75853d47cc2716ab6
franncolom <franciscocolom000@gmail.com>
2025-04-03 14:15:32 -0300

    Agregando operadores.php

---
+++ operadores.php
@@ -0,0 +1,21 @@
+<?php 
+
+$edad = 1;
+$edad += 1;
+
+echo $edad;
+
+$edad = 1;
+$edad -= 10;
+echo $edad;  
+
+/* 
+  = Asignación
+  += Suma y asignación
+  -= Resta y asignación
+  *= Multiplicación y asignación
+  /= División y asignación
+  %= Modulo y asignación
+  .= Concatenación y asignación
+*/
+?>
