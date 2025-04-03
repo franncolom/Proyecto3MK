b4dc1d75b0b9c07bb6efc353c9871c10753b69f3
franncolom <franciscocolom000@gmail.com>
2025-04-03 14:02:14 -0300

    Agregando concatenacion.php

---
+++ concatenacion.php
@@ -0,0 +1,9 @@
+<?php
+$emisora = 'Radio Disney';
+echo "Me gusta escuchar $emisora";
+
+/*
+  En echo donde se llame a una variable, el echo debe ir con comillas dobles,
+  y no con comillas simples.
+*/
+?>
